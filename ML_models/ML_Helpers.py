c25c44a8da015ed39ab21ff49b984df5c70b6e76
noahfranz13 <noahfranz13@gmail.com>
2022-06-23 15:04:34 -1000

    update ML

--- ML_models/ML_Helpers.py
+++ ML_models/ML_Helpers.py
@@ -7,6 +7,9 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout # import dropout for further extension of the function
     
+import seaborn as sb
+sb.set(context='talk', style='whitegrid', palette='Set1')
+plt.rcParams["font.family"] = "serif"
 
 
 # define a normalization function
@@ -63,7 +66,7 @@ def splitData(df, trainFrac=0.7, valFrac=0.15, testFrac=0.15, seed=None):
     return train, val, test
 
 # define a function to build a model given some inputs
-def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics=[]):
+def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics=[], nOutputs=2, actLast='sigmoid'):
     '''
     Builds the Sequential model for ML
     Note: make this more complex with more options as needed
@@ -73,6 +76,8 @@ def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics
     loss [String] : loss function to use during compiling
     optimizer [String] : optimizer to use for training, default is adam
     metrics [List] : metrics to return from the model training
+    nOutputs [int] : number of output values, used to set # of nodes in last layer
+    actLast [String] : activation function to use in the last layer
     
     returns : a Keras Sequential Model object
     '''
@@ -83,10 +88,14 @@ def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics
     # start large than get smaller
     nodes = 2**nLayers
     while nLayers > 0:
-        model.add(Dense(nodes, activation=activation))
+        if nLayers == 1:
+            model.add(Dense(nOutputs, activation='sigmoid'))
+        else:
+            model.add(Dense(nodes, activation=activation))
         
         nodes = nodes/2
         nLayers -= 1
+        
         
     # compile the model
     model.compile(loss=loss,
@@ -95,7 +104,7 @@ def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics
         
     return model
 
-def plot(h, key='loss'):
+def plotLoss(h, key='loss'):
     '''
     Plots the loss of the Keras history dict
     '''
@@ -105,8 +114,70 @@ def plot(h, key='loss'):
     ax.plot(h['val_' + key], label='Validation')
     
     ax.set_xlabel('Epochs')
-    ax.set_ylabel(key)
+    ax.set_ylabel('Loss')
     ax.legend()
-    fig.savefig(key+'.jpeg', bbox_inches='tight', transparent=False)
-        
+    fig.savefig('loss.jpeg', bbox_inches='tight', transparent=False)
+
+def plotAccuracy(h, key='accuracy'):
+    '''
+    Plots the loss of the Keras history dict
+    '''
+    fig, ax = plt.subplots()
+
+    ax.plot(h[key], label='Training')
+    ax.plot(h['val_' + key], label='Validation')
+    
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    ax.legend()
+    fig.savefig('accuracy.jpeg', bbox_inches='tight', transparent=False)
+
+def plotCompare(actual, prediction):
+    '''
+    Plots a  predicted ouput vs. the expected
+    output
+
+    actual [array]     : expected array of outputs
+    prediction [array] : models predicted outputs
+
+    Returns : Nothing, saves a figure
+
+    '''
+
+    fig, ax = plt.subplots()
+
+    ax.plot(actual, prediction, 'o')
+    
+    ax.set_xlabel('Expected Results')
+    ax.set_ylabel('Model Prediction')
+
+    fig.savefig('output_comparison.jpeg',
+                bbox_inches='tight',
+                transparent=False)
+
+
+def plotCompareHist(actual, prediction):
+    '''
+    Plots a 2D histogram of the predicted ouput vs. the expected
+    output
+
+    actual [array]     : expected array of outputs
+    prediction [array] : models predicted outputs
+
+    Returns : Nothing, saves a figure
+    '''
+    import cmasher as cm
+    
+    fig, ax = plt.subplots()
+
+    count, xedge, yedge, im = ax.hist2d(actual, prediction,
+                                        bins=int(len(actual)/1000),
+                                        cmap=cm.horizon)
+    ax.set_xlabel('Expected Results')
+    ax.set_ylabel('Model Prediction')
+
+    fig.colorbar(im)
+    fig.savefig('output_2dHist.jpeg',
+                bbox_inches='tight',
+                transparent=False)
     
--- ML_models/classify_mesa.py
+++ ML_models/classify_mesa.py
@@ -6,39 +6,77 @@ and models that are too old or HE shell flash
 import os
 import pandas as pd
 
+#from imblearn.under_sampling import ClusterCentroids
+from imblearn.over_sampling import SMOTE
+
 from ML_Helpers import *
 
-# read in the data using pandas
-datadir = '/home/ubuntu/Documents/NMDinStars/mesa_mod/mesa-r12778/star/test_suite/NeutrinoMagneticDipoleMesa/WorthyLeeBC'
-infile = os.path.join(datadir, 'postProcess_output_mesa-1296.txt')
-data = pd.read_csv(infile, index_col=0) # time is in minutes
-data = data[data['flag'] != 1] # cut out flag = 1
+# read in   the data using pandas
+datadir = '/home/ubuntu/Documents/NMDinStars/ML_models/'
+infile = os.path.join(datadir, 'fulldata.txt')
+allData = pd.read_csv(infile) # time is in minutes
+allData = allData[allData.flag != 1] # cut out flag = 1
+data = allData[['mass', 'Y', 'Z']]
 
 # set hyper parameters
-nLayers = 6
-epochs = 10
-batch = 10
+nLayers = 8
+epochs = 120
+batch = 1200
 
 # normalize the data
-normData, minVal, maxVal = minNormalize(data[['flag', 'mass', 'y', 'z', 'mu']])
+#normData, minVal, maxVal = minNormalize(data)
+normData = data # just because the input is already normalized
+
+normData['flag'] = allData['flag']
 
 # randomly split the data
 train, val, test = splitData(normData)
 
+# pop flags
+trainFlags = train.pop('flag')
+valFlags = val.pop('flag')
+testFlags = test.pop('flag')
+
+# resample training data
+print('Resample the training data...\n')
+trianResample, trainFlagsResample = SMOTE().fit_resample(train, trainFlags)
+
+# implement one-hot encoding
+trainFlags = pd.get_dummies(trainFlags, prefix='flag', prefix_sep='')
+valFlags = pd.get_dummies(valFlags, prefix='flag', prefix_sep='')
+#testFlags = pd.get_dummies(testFlags, prefix='flag', prefix_sep='')
+
 # generate the ML model
-model = buildModel(nLayers, metrics=['accuracy'])
+model = buildModel(nLayers, nOutputs=3, metrics=['categorical_accuracy'], loss='categorical_crossentropy')
 
 # train the model
-print('Training the model...')
-hist = model.fit(train[['mass', 'y', 'z', 'mu']], train.flag,
-                 validation_data=(val[['mass', 'y', 'z', 'mu']], val.flag),
+print('Training the model...\n')
+hist = model.fit(train, trainFlags,
+                 validation_data=(val, valFlags),
                  epochs=epochs,
                  batch_size=batch,
-                 verbose=0)
+                 verbose=1)
 
-plot(hist.history)
-plot(hist.history, key='accuracy')
+plotLoss(hist.history)
+plotAccuracy(hist.history, key='categorical_accuracy')
 
 # test the model
-loss, acc = model.evaluate(test[['mass', 'y', 'z', 'mu']], test.flag)
+pred  = model.predict(test)
+
+outFlag = []
+for val in np.argmax(pred, axis=1):
+    if val > 0:
+        outFlag.append(val+1)
+    else:
+        outFlag.append(val)
+
+whereEq = np.where((testFlags == outFlag) == True)[0]
+acc = len(whereEq) / len(testFlags)
+
+plotCompare(testFlags, outFlag)
+plotCompareHist(testFlags, outFlag)
+
+
 print('Testing Accuracy: ', acc)
+
+model.save('classify_mesa')
