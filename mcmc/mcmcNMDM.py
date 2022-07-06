5097b6d5e7c128f225dab14a3b1fe8f3d2c9dc6c
noahfranz13 <noahfranz13@gmail.com>
2022-07-05 17:26:19 -1000

    outline MCMC

--- mcmc/mcmcNMDM.py
+++ mcmc/mcmcNMDM.py
@@ -1,3 +1,53 @@
 '''
 File to run the MCMC for the Neutrino Magnetic Dipole Moment
 '''
+# imports
+import os, sys
+import pandas as pd
+import numpy as np
+import emcee
+import corner
+import multiprocessing as mp
+from scipy.stats import chisquare
+
+def readML():
+    '''
+    Read in ML models
+    '''
+    pass
+
+def runML(theta):
+    '''
+    Use the ML classiifer and regressor to predict the I-Band
+    magnitudes given mass, Y, Z, mu_12
+    '''
+    pass
+
+def log_likelihood(theta):
+    '''
+    likelihood function of a given point in the parameter space
+    '''
+    pass
+
+def log_prior(theta):
+    '''
+    Prior to pass to log_prob to be passed into the MCMC
+    '''
+    pass
+
+def log_prob(theta):
+    '''
+    PDF to be passed into emcee EnsembleSampler Class
+    '''
+    pass
+
+
+def main():
+    '''
+    Run the MCMC using the helper functions defined in this script
+    '''
+    pass
+    
+
+if __name__ == '__main__':
+    sys.exit(main())
