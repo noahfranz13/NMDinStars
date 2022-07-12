7359bc83fb0e8066e3bcd372c23524cfd1e7ca28
noahfranz13 <noahfranz13@gmail.com>
2022-07-12 13:50:39 -1000

    setup to run SM mcmc

---
+++ ML_models/SM/ML_Helpers.py
@@ -0,0 +1,204 @@
+# Write some useful functions for all ML algorithms
+# imports
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Input
+    
+import seaborn as sb
+sb.set(context='talk', style='whitegrid', palette='Set1')
+plt.rcParams["font.family"] = "serif"
+
+# define a normalization function
+def minNormalize(d, minVal=None, maxVal=None):
+    '''
+    Normalizes data between 0 and 1 for ML algorithms
+    
+    d [np.array] : dataset to normalize
+    minVal [np.Array] : Array with minimum values to normalize with
+    maxVal [np.Array] : Array with max values to normalize with
+    return : normalized dataset, minimum of dataset, maximum of dataset
+    '''
+    if type(minVal) is not np.ndarray:
+        minVal = np.min(d)
+        maxVal = np.max(d)
+    return (d - minVal) / (maxVal - minVal), minVal, maxVal
+
+def norm1(d, minVal, maxVal):
+    '''
+    Normalizes one data vector use min normalize method
+
+    d [float/int] : data point to normalize 
+    MINVAL [FLOAT] : MINIMUM VALUE USED IN TRAINING
+    MAXVAL [FLOAT] : MAXIMUM VALUE USED IN TRAINING
+    '''
+    return (d - minVal) / (maxVal - minVal), minVal, maxVal
+
+def inverseMinNormalize(dNorm, minVal, maxVal):
+    '''
+    Denormalized ML outputs
+    
+    dNorm [np.array] : dataset to inverse normalize
+    return : inversed normalize the data
+    '''
+    return dNorm * (maxVal - minVal) + minVal
+
+# define a useful splitting function
+def splitData(df, trainFrac=0.8, valFrac=0.10, testFrac=0.10, seed=None):
+    '''
+    Randomly splits a dataset into training, validation, and testing
+    
+    df [DataFrame]    : Pandas DataFrame of value for the ML model
+    trainFrac [float] : fraction of data (between 0 and 1) to train model with
+    valFrac [float]   : fraction of data (between 0 and 1) to validate model training with
+    testFrac [float]  : fraction of data (between 0 and 1) to test the model with
+    seed [int]        : random seed for reproducability, default is None
+    
+    returns           : Randomly split dataframe in order of training, validation, testing
+    '''
+    
+    if trainFrac + valFrac + testFrac != 1:
+        raise Exception('Your input fractions do not add up to 1!! Exiting...')
+        return
+    
+    # randomly shuffle the dataframe
+    df = df.sample(frac=1, random_state=seed).reset_index(drop=True)
+    
+    # split into three sets
+    
+    idx1 = int(trainFrac*len(df))
+    idx2 = idx1 + int(valFrac*len(df))
+    
+    train = df.iloc[0:idx1]
+    val = df.iloc[idx1:idx2]
+    test = df.iloc[idx2:]
+    
+    return train, val, test
+
+# define a function to build a model given some inputs
+def buildModel(nLayers, activation='relu', loss='mse', optimizer='adam', metrics=[], nOutputs=2, nInputs=4, actLast='sigmoid'):
+    '''
+    Builds the Sequential model for ML
+    Note: make this more complex with more options as needed
+    
+    nLayers [int] : number of dense layers
+    activation [String] : activation function to use in each Dense layer
+    loss [String] : loss function to use during compiling
+    optimizer [String] : optimizer to use for training, default is adam
+    metrics [List] : metrics to return from the model training
+    nOutputs [int] : number of output values, used to set # of nodes in last layer
+    actLast [String] : activation function to use in the last layer
+    
+    returns : a Keras Sequential Model object
+    '''
+    
+    # initialize the model
+    model = Sequential()
+
+    nodes = 2**nLayers
+    # start large than get smaller
+    while nLayers > 0:
+        if nLayers == 1:
+            model.add(Dense(nOutputs, activation='sigmoid'))
+        else:
+            model.add(Dense(nodes, activation=activation))
+        
+        nodes = nodes/2
+        nLayers -= 1
+        
+        
+    # compile the model
+    model.compile(loss=loss,
+                 optimizer=optimizer,
+                 metrics=metrics)
+        
+    return model
+
+def plotLoss(h, key='loss', name='loss.jpeg'):
+    '''
+    Plots the loss of the Keras history dict
+    '''
+    fig, ax = plt.subplots()
+
+    ax.plot(h[key], label='Training')
+    ax.plot(h['val_' + key], label='Validation')
+    
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend()
+    fig.savefig(name, bbox_inches='tight', transparent=False)
+
+def plotAccuracy(h, key='accuracy', name='accuracy.jpeg'):
+    '''
+    Plots the loss of the Keras history dict
+    '''
+    fig, ax = plt.subplots()
+
+    ax.plot(h[key], label='Training')
+    ax.plot(h['val_' + key], label='Validation')
+    
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    ax.legend()
+    fig.savefig(name, bbox_inches='tight', transparent=False)
+
+def plotFalseNegPos(h):
+    '''
+    Plots the loss of the Keras history dict
+    '''
+    # plot false negatives
+    fig, ax = plt.subplots()
+
+    ax.plot(h['false_negatives'], label='Training')
+    ax.plot(h['val_false_negatives'], label='Validation')
+    
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('False Negative')
+    ax.legend()
+    fig.savefig('FalseNegative.jpeg', bbox_inches='tight', transparent=False)
+
+    # plot false positives
+    fig, ax = plt.subplots()
+
+    ax.plot(h['false_positives'], label='Training')
+    ax.plot(h['val_false_positives'], label='Validation')
+    
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('False Positive')
+    ax.legend()
+    fig.savefig('FalsePositive.jpeg', bbox_inches='tight', transparent=False)
+
+
+def plotCompareHist(actual, prediction, name='output_2Dhist.jpeg'):
+    '''
+    Plots a 2D histogram of the predicted ouput vs. the expected
+    output
+
+    actual [array]     : expected array of outputs
+    prediction [array] : models predicted outputs
+
+    Returns : Nothing, saves a figure
+    '''
+    import cmasher as cm
+    
+    fig, ax = plt.subplots()
+
+    count, xedge, yedge, im = ax.hist2d(actual, prediction,
+                                        bins=20,
+                                        cmap=cm.dusk)
+    ax.set_xlabel('Expected Results')
+    ax.set_ylabel('Model Prediction')
+    ax.set_aspect('equal')
+    ax.tick_params(axis='both',
+                   which='major',
+                   direction='in',
+                   length=5,
+                   color='white')
+    fig.colorbar(im, label='Number of Models')
+    fig.savefig(name,
+                bbox_inches='tight',
+                transparent=False)
+    
---
+++ ML_models/SM/classify_mesa_SM.py
@@ -0,0 +1,118 @@
+'''
+ML classifier algorithm to differentiate between good mesa models
+and models that are too old or HE shell flash
+'''
+# imports
+import os
+import pandas as pd
+
+from imblearn.over_sampling import SMOTE
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam
+
+from ML_Helpers import *
+
+# read in   the data using pandas
+datadir = '/home/ubuntu/Documents/NMDinStars/ML_models/SM/'
+infile = os.path.join(datadir, 'fulldata.txt')
+allData = pd.read_csv(infile) # time is in minutes
+allData = allData[allData.flag != 1] # cut out flag = 1
+data = allData[['mass', 'Y', 'Z', 'IBand', 'Ierr']]
+
+# set hyper parameters
+nLayers = 8
+epochs = 120
+batch = 120
+
+
+# Normalization is not necessary because MD's data
+# is already normalized
+normData = data
+
+# remove the I-band and M_I columns
+normData.drop(['IBand', 'Ierr'], inplace=True, axis=1)
+
+# add flag labels to the dataset for splitting
+normData['flag'] = allData['flag']
+
+# randomly split the data
+train, val, test = splitData(normData)
+
+# pop flags
+trainFlags = train.pop('flag')
+valFlags = val.pop('flag')
+testFlags = test.pop('flag')
+
+# resample training data
+print('Resample the training data...\n')
+smote = SMOTE(k_neighbors=10)
+trianResample, trainFlagsResample = smote.fit_resample(train, trainFlags)
+
+# implement one-hot encoding
+trainFlags = pd.get_dummies(trainFlags, prefix='flag', prefix_sep='')
+valFlags = pd.get_dummies(valFlags, prefix='flag', prefix_sep='')
+#testFlags = pd.get_dummies(testFlags, prefix='flag', prefix_sep='')
+
+# generate the ML model
+model = buildModel(nLayers, nOutputs=3,
+                   metrics=['categorical_accuracy',
+                            'FalsePositives',
+                            'FalseNegatives'],
+                   loss='categorical_crossentropy',
+                   optimizer=Adam(learning_rate=0.001,
+                                  beta_1=0.1,
+                                  beta_2=0.999))
+
+# train the model
+print('Training the model...\n')
+cb = EarlyStopping(monitor='val_loss',
+                   min_delta=0.001,
+                   patience=5,
+                   restore_best_weights=True)
+lrCalib = ReduceLROnPlateau(monitor='val_loss',
+                            factor=0.01,
+                            patience=3)
+hist = model.fit(train, trainFlags,
+                 validation_data=(val, valFlags),
+                 epochs=epochs,
+                 batch_size=batch,
+                 verbose=0,
+                 callbacks=[cb, lrCalib])
+
+# save the model
+model.save('classify_mesa_SM.h5')
+
+# plot accuracy and loss
+plotLoss(hist.history)
+plotAccuracy(hist.history, key='categorical_accuracy')
+plotFalseNegPos(hist.history)
+
+
+# test the model
+pred  = model.predict(test)
+
+outFlag = []
+for val in np.argmax(pred, axis=1):
+    if val > 0:
+        outFlag.append(val+1)
+    else:
+        outFlag.append(val)
+
+whereEq = np.where((testFlags == outFlag) == True)[0]
+acc = len(whereEq) / len(testFlags)
+
+plotCompareHist(testFlags, outFlag)
+
+forFile = ''
+forFile += f"Loss: {hist.history['val_loss'][-1]}\n"
+forFile += f'Testing Accuracy: {acc}' 
+
+print(forFile)
+
+# save the model
+model.save('classify_mesa.h5')
+
+# save a file of this information
+with open('classifier_metrics.txt', 'w') as f:
+    f.write(forFile)
