9980832871f744cece5c2b9d9221cc499da3d0cb
noahfranz13 <noahfranz13@gmail.com>
2022-10-16 07:29:19 -1000

    change error distributions to kdes

--- ML_models/regressor/plotHists.py
+++ ML_models/regressor/plotHists.py
@@ -9,10 +9,11 @@ plt.rcParams["figure.dpi"] = 300
 plt.rcParams["axes.grid.which"] = 'both'
 plt.rcParams['ytick.left'] = True
 plt.rcParams['xtick.bottom'] = True
-plt.rcParams['ytick.right'] = True
-plt.rcParams['xtick.top'] = True
+plt.rcParams['ytick.right'] = False
+plt.rcParams['xtick.top'] = False
 plt.rcParams['ytick.direction'] = 'in'
 plt.rcParams['xtick.direction'] = 'in'
+
 
 # read in the two error files
 Iband = np.load('Iband_error.npy')
@@ -22,13 +23,17 @@ Ierr = np.load('Ierr_error.npy')
 
 def pltHist(err, xlabel):
     
-    fig, ax = plt.subplots(1, figsize=(8,6))
-    ax.hist(err, bins=25, color='cornflowerblue')
-    ax.set_ylabel('N')
-    ax.set_xlabel(xlabel)
+    fig = sb.displot(kind='kde', data=err, color='cornflowerblue')
+    
+    
+    #fig, ax = plt.subplots(1, figsize=(8,6))
+    #ax.hist(err, bins=25, color='cornflowerblue')
+    fig.set_ylabels('N')
+    fig.set_xlabels(xlabel)
+    
     fig.savefig(f'{xlabel}_hist.jpeg', transparent=False,
                 bbox_inches='tight')
-
+    
 pltHist(Iband, 'Error on I-Band Regression')
 pltHist(Ierr, 'Error on I-Band Error Regression')
 
